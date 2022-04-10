2267bcce3fdc8c89c7b789be390a4752540aeb01
JulienLEUILLIER <julien.leuillier@gmail.com>
2022-04-10 13:35:46 +0200

    Connect to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const colors = require("colors");
 const dotenv = require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 const { errorHandler } = require("./middleware/errorHandler");
+const connectDB = require("./config/db");
+
+connectDB();
 
 app.use([express.json(), express.urlencoded({ extended: false })]);
 
