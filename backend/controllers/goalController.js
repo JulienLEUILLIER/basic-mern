564235ecbee31641a25a50aea5676bd402e64003
JulienLEUILLIER <julien.leuillier@gmail.com>
2022-04-10 12:32:38 +0200

    try catch for error handler

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -2,13 +2,15 @@ const getGoals = (req, res) => {
   res.status(200).send({ message: "Get goals" });
 };
 
-const setGoal = (req, res) => {
-  if (!req.body.text) {
-    res.status(400);
-    throw new Error("Please input a text field");
+const setGoal = (req, res, next) => {
+  try {
+    if (!req.body.text) {
+      throw new Error("Please input a text field");
+    }
+    res.send({ message: "Set goal" });
+  } catch (err) {
+    return next(err);
   }
-
-  res.status(200).send({ message: "Set goals" });
 };
 
 const updateGoal = (req, res) => {
